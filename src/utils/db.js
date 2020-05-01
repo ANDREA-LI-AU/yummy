5d56c81c147b6e16f409460acf212781d74a73d1
Andrea Li <andrea-li-au@users.noreply.github.com>
2020-05-01 15:36:41 +1000

    deploy setup

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 
 exports.connectToDB = () => {
-    const {DB_DATABASE, DB_HOST, DB_PORT} =  process.env;
-    const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+    const {DB_DATABASE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD} =  process.env;
+    let connectionString;
+    if (DB_USER && DB_PASSWORD){
+        connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`;
+    } else{
+        connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+    }
+    console.log(connectionString);
     return mongoose.connect(connectionString, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true ,
         useFindAndModify: false
     });
+
+    
 }
